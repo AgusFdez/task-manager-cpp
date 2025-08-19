b0a3436d4c55869240300cd4e01a04fafcae431f
AgusFdez <agusfdez329@gmail.com>
2025-08-19 12:31:17 -0300

    Versión 1: menú con bucle y agregar/listar tareas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 int main()
-{
+{    
+    int opcion;
+
+    while (opcion != 3) {
+        std::cout << " === Gestor de Tareas === " << std::endl;
+        std::cout << "1. Agregar tarea" << std::endl;
+        std::cout << "2. Listar tareas" << std::endl;
+        std::cout << "3. Salir" << std::endl;
+
+        std::cout << "Opcion: ";
+
+         std::cin >> opcion; 
+        }
+
+
+
     return 0;
 }
