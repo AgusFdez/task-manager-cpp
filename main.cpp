2d64a9d7131633f2ece0a2f06a67bafa00fa6ce8
AgusFdez <agusfdez329@gmail.com>
2025-08-21 10:07:14 -0300

    Versión 2: opción de agregar tarea lista.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,68 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
+#include <string>
 using namespace std;
+
+class Tarea {
+    private:
+        int id;
+        std::string descripcion;
+        bool estado;
+
+    public:
+        //constructor
+        Tarea(int id, std::string descripcion)
+            : id(id), descripcion(descripcion), estado(false) {}
+
+        //metodos
+        void marcarestado() {estado = true; }
+        void mostrar() const{
+            cout << "[" << id << "] " << descripcion
+                 << (estado ? " (Completada)" : " (Pendiente)") << endl;
+        }
+
+        //getters
+        int getId() const { return id; }
+        std::string getDescripcion() const { return descripcion;}
+        bool estaestado() const { return estado; }
+};
 
 int main()
 {    
-    int opcion;
+    int opcion = 0;
+    int ultimoId = 0;
+    std::vector<Tarea> tareas;
 
     while (opcion != 3) {
-        std::cout << " === Gestor de Tareas === " << std::endl;
-        std::cout << "1. Agregar tarea" << std::endl;
-        std::cout << "2. Listar tareas" << std::endl;
-        std::cout << "3. Salir" << std::endl;
+        system("CLS");
 
-        std::cout << "Opcion: ";
+        switch(opcion){
+        case 0: {
+            std::cout << " === Gestor de Tareas === " << std::endl;
+            std::cout << "1. Agregar tarea" << std::endl;
+            std::cout << "2. Listar tareas" << std::endl;
+            std::cout << "3. Salir" << std::endl << std::endl;
+            std::cout << "Opcion: ";
+            std::cin >> opcion;
+            std::cin.ignore();
+        } break;
 
-         std::cin >> opcion; 
+        case 1: {
+            std::string desc;
+            std::cout << " Describa la tarea: ";
+            getline(std::cin, desc);
+            ultimoId++;
+            Tarea tarea(ultimoId, desc);
+            tareas.push_back(tarea);
+            opcion = 0;
+        } break;
+        
+        
         }
+ 
+
+    }
 
 
 
